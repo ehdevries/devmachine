a6094411d42d70c083dad053c5ec6822c4bb5644
Edward DeVries <ehdevries@gmail.com>
2017-05-05 13:37:49 -0400

    Install software with Chocolatey
    Also download software that either isn't available
    from Chocolatey or has an out of date package.

---
+++ scripts/1_install.ps1
@@ -0,0 +1,25 @@
+### Install handy tools and apps for my dev machine
+### Run this script from an administrative shell
+
+Set-ExecutionPolicy RemoteSigned
+
+# Install Chocolatey
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install some software
+choco install -y 7zip
+choco install -y git
+#choco install -y github        # The GitHub Desktop Chocolatey package is significantly behind as of May 2017, so I'll install it manually
+choco install -y gitkraken
+choco install -y googlechrome
+choco install -y hyper
+choco install -y keepass
+choco install -y linqpad
+choco install -y nodejs-lts
+choco install -y notepadplusplus
+choco install -y poshgit
+choco install -y rdcman
+choco install -y slack
+#choco install -y toggl         # The Toggl Desktop Chocolatey package is significantly behind as of May 2017, so I'll install it manually
+choco install -y visualstudiocode
+RefreshEnv.cmd
---
+++ scripts/2_download.ps1
@@ -0,0 +1,9 @@
+### Download anything that we didn't install with Chocolatey so that it can be installed manually
+### This script does not require an administrative shell
+
+$downDir = New-Item -Path $([Environment]::GetFolderPath("Desktop")) -Name "to_install" -ItemType Directory -Force
+$webClient = New-Object System.Net.WebClient
+
+$webClient.DownloadFile("https://github.com/bruderstein/nppPluginManager/releases/download/v1.4.9/PluginManager_v1.4.9_x64.zip", "$downDir\nppPluginManager.zip")
+$webClient.DownloadFile("https://github-windows.s3.amazonaws.com/GitHubSetup.exe", "$downDir\GitHubSetup.exe")
+$webClient.DownloadFile("https://www.toggl.com/api/v8/installer?app=td&platform=windows&channel=stable", "$downDir\TogglDesktopInstaller.exe")
