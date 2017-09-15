995284d2e2361d97f674cfbc2dde3a6e2c01c1b2
Edward DeVries <ehdevries@gmail.com>
2017-09-15 10:03:37 -0400

    Tweak software installation methods
    Download VS Code from direct link because
    it gets such frequent updates.
    
    Get the new Github Desktop beta with Electron.
    
    Get the current Node version instead of LTS
    in order to give Hyper and MSSQL a better shot.
    
    Don't bother with the Notepad++ plugin manager
    because it's on the roadmap to be baked in.

--- scripts/2_download.ps1
+++ scripts/2_download.ps1
@@ -4,6 +4,6 @@
 $downDir = New-Item -Path $([Environment]::GetFolderPath("Desktop")) -Name "to_install" -ItemType Directory -Force
 $webClient = New-Object System.Net.WebClient
 
-$webClient.DownloadFile("https://github.com/bruderstein/nppPluginManager/releases/download/v1.4.9/PluginManager_v1.4.9_x64.zip", "$downDir\nppPluginManager.zip")
-$webClient.DownloadFile("https://github-windows.s3.amazonaws.com/GitHubSetup.exe", "$downDir\GitHubSetup.exe")
+$webClient.DownloadFile("https://central.github.com/deployments/desktop/desktop/latest/win32", "$downDir\GitHubDesktopSetup.exe")
 $webClient.DownloadFile("https://www.toggl.com/api/v8/installer?app=td&platform=windows&channel=stable", "$downDir\TogglDesktopInstaller.exe")
+$webClient.DownloadFile("https://go.microsoft.com/fwlink/?Linkid=852157", "$downDir\VSCodeSetup.exe")
