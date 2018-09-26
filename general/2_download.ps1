82ed4927a182ae847f386f453fb3cdeba53ecf65
Edward DeVries <ehdevries@gmail.com>
2018-09-26 11:54:50 -0400

    Replace SqlOps with Azure Data Studio

--- general/2_download.ps1
+++ general/2_download.ps1
@@ -4,6 +4,7 @@
 $downDir = New-Item -Path $([Environment]::GetFolderPath("Desktop")) -Name "to_install" -ItemType Directory -Force
 $webClient = New-Object System.Net.WebClient
 
+$webClient.DownloadFile("https://go.microsoft.com/fwlink/?linkid=2024683",                               "$downDir\AzureDataStudioSetup.exe")
 $webClient.DownloadFile("https://download.mozilla.org/?product=firefox-stub&os=win&lang=en-US",          "$downDir\FirefoxSetup.exe")
 $webClient.DownloadFile("https://central.github.com/deployments/desktop/desktop/latest/win32",           "$downDir\GitHubDesktopSetup.exe")
 $webClient.DownloadFile("https://www.gitkraken.com/download/windows64",                                  "$downDir\GitKrakenSetup.exe")
@@ -11,7 +12,6 @@ $webClient.DownloadFile("https://releases.hyper.is/download/win",               
 $webClient.DownloadFile("https://insomnia.rest/download/#windows",                                       "$downDir\InsomniaSetup.exe")
 $webClient.DownloadFile("https://app.getpostman.com/app/download/win64",                                 "$downDir\PostmanSetup.exe")
 $webClient.DownloadFile("https://slack.com/ssb/download-win64",                                          "$downDir\SlackSetup.exe")
-$webClient.DownloadFile("https://go.microsoft.com/fwlink/?linkid=875602",                                "$downDir\SqlOpsSetup.exe")
 $webClient.DownloadFile("https://www.toggl.com/api/v8/installer?app=td&platform=windows&channel=stable", "$downDir\TogglDesktopSetup.exe")
 $webClient.DownloadFile("https://aka.ms/win32-x64-user-stable",                                          "$downDir\VSCodeSetup.exe")
 
