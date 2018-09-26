ad6456b172a57353505fee8e3703ac100b726f06
Edward DeVries <ehdevries@gmail.com>
2018-09-26 12:01:18 -0400

    Format commands for visual clarity

--- general/3_configure.ps1
+++ general/3_configure.ps1
@@ -24,9 +24,9 @@ code --install-extension PKief.material-icon-theme
 # Copy config files to their normal directories
 Write-Output ""
 Write-Output "Restoring the following configuration files"
-Copy-Item -Path "$configDir\.gitconfig" -Destination $home -PassThru | Split-Path -Leaf
-Copy-Item -Path "$configDir\.hyper.js" -Destination $home -PassThru | Split-Path -Leaf
+Copy-Item -Path "$configDir\.gitconfig"  -Destination $home -PassThru | Split-Path -Leaf
+Copy-Item -Path "$configDir\.hyper.js"   -Destination $home -PassThru | Split-Path -Leaf
 Copy-Item -Path "$configDir\servers.rdg" -Destination $home -PassThru | Split-Path -Leaf
-Get-ChildItem -Path $configDir\SqlOps\*.json | Copy-Item -Destination "$env:APPDATA\azuredatastudio\User" -PassThru | Split-Path -Leaf
-Get-ChildItem -Path $configDir\VSCode\*.json | Copy-Item -Destination "$env:APPDATA\Code\User" -PassThru | Split-Path -Leaf
+Get-ChildItem -Path $configDir\AzureDataStudio\*.json  | Copy-Item -Destination "$env:APPDATA\azuredatastudio\User" -PassThru | Split-Path -Leaf
+Get-ChildItem -Path $configDir\VSCode\*.json           | Copy-Item -Destination "$env:APPDATA\Code\User"            -PassThru | Split-Path -Leaf
 Get-ChildItem -Path $configDir\WindowsPowerShell\*.ps1 | Copy-Item -Destination "$home\Documents\WindowsPowerShell" -PassThru | Split-Path -Leaf
