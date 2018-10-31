0646da9a520ca9c53857aa417102de8048fa0705
Edward DeVries <ehdevries@gmail.com>
2018-10-31 10:22:51 -0400

    Drop Postman in favor of Insomnia

--- general/2_download.ps1
+++ general/2_download.ps1
@@ -11,7 +11,6 @@ $webClient.DownloadFile("https://central.github.com/deployments/desktop/desktop/
 $webClient.DownloadFile("https://www.gitkraken.com/download/windows64",                                  "$downDir\GitKrakenSetup.exe")
 $webClient.DownloadFile("https://releases.hyper.is/download/win",                                        "$downDir\HyperSetup.exe")
 $webClient.DownloadFile("https://insomnia.rest/download/#windows",                                       "$downDir\InsomniaSetup.exe")
-$webClient.DownloadFile("https://app.getpostman.com/app/download/win64",                                 "$downDir\PostmanSetup.exe")
 $webClient.DownloadFile("https://slack.com/ssb/download-win64",                                          "$downDir\SlackSetup.exe")
 $webClient.DownloadFile("https://www.toggl.com/api/v8/installer?app=td&platform=windows&channel=stable", "$downDir\TogglDesktopSetup.exe")
 $webClient.DownloadFile("https://aka.ms/win32-x64-user-stable",                                          "$downDir\VSCodeSetup.exe")
