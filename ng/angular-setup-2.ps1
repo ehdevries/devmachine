92d76937cfc194113f7a78ce4e84ba2f581ecb51
Edward DeVries <ehdevries@gmail.com>
2018-12-22 16:31:25 -0500

    Remove Angular setup scripts
    They're not that useful for me,
    and easy to reproduce if needed.

--- ng/angular-setup-2.ps1
+++
@@ -1,6 +0,0 @@
-### Install and configure some handy tools for building Angular applications - part 2
-### This script does not require an administrative shell
-
-Write-Output ""
-Write-Output "Installing VS Code extensions for Angular development"
-code --install-extension johnpapa.angular-essentials
