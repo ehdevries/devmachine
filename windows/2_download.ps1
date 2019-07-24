ec06143d26456dda2818d8c2d63ebffd49239362
Edward DeVries <ehdevries@gmail.com>
2019-07-24 14:55:40 -0400

    Don't install GitKraken
    I think I'd rather use SSH keys and the CLI

--- windows/2_download.ps1
+++ windows/2_download.ps1
@@ -8,7 +8,6 @@ $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile("https://go.microsoft.com/fwlink/?linkid=2049972",                               "$downDir\AzureDataStudioSetup.exe")
 $webClient.DownloadFile("https://download.mozilla.org/?product=firefox-stub&os=win&lang=en-US",          "$downDir\FirefoxSetup.exe")
 $webClient.DownloadFile("https://central.github.com/deployments/desktop/desktop/latest/win32",           "$downDir\GitHubDesktopSetup.exe")
-$webClient.DownloadFile("https://www.gitkraken.com/download/windows64",                                  "$downDir\GitKrakenSetup.exe")
 $webClient.DownloadFile("https://releases.hyper.is/download/win",                                        "$downDir\HyperSetup.exe")
 $webClient.DownloadFile("https://insomnia.rest/download/#windows",                                       "$downDir\InsomniaSetup.exe")
 $webClient.DownloadFile("https://slack.com/ssb/download-win64",                                          "$downDir\SlackSetup.exe")
