821454d2d6dc391e8e91c3d8ad36a9d194d005c7
Edward DeVries <ehdevries@gmail.com>
2020-02-20 22:44:24 -0500

    Don't install Debugger for Chrome extension

--- windows/3_configure.ps1
+++ windows/3_configure.ps1
@@ -29,7 +29,6 @@ code --install-extension eg2.vscode-npm-script
 code --install-extension firefox-devtools.vscode-firefox-debug
 code --install-extension glen-84.sass-lint
 code --install-extension johnpapa.angular-essentials
-code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension nrwl.angular-console
 
 # Windows
