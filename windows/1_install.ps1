08895e02fcc7c3fd1baa87579940442a129359bb
Edward DeVries <ehdevries@gmail.com>
2020-06-22 17:24:06 -0400

    Don't install 7zip

--- windows/1_install.ps1
+++ windows/1_install.ps1
@@ -6,7 +6,6 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install -y 7zip
 choco install -y dejavufonts
 choco install -y firacode
 choco install -y git
