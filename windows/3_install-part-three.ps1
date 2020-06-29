8088aeca9ae8c9f15191f04b1f17c43de06387f2
Edward DeVries <ehdevries@gmail.com>
2020-06-29 12:23:55 -0400

    Move installation from step 4 to step 3

--- windows/3_install-part-three.ps1
+++ windows/3_install-part-three.ps1
@@ -4,3 +4,54 @@ Write-Output ""
 Write-Output "Installing posh-git for Windows PowerShell"
 Write-Output ""
 PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
+
+Write-Output ""
+Write-Output "Installing VS Code extensions"
+Write-Output ""
+
+# Common
+code --install-extension coenraads.bracket-pair-colorizer-2
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension eamodio.gitlens
+code --install-extension editorconfig.editorconfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension johnpapa.vscode-peacock
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension pkief.material-icon-theme
+code --install-extension robbowen.synthwave-vscode
+
+# .NET
+code --install-extension adrianwilczynski.user-secrets
+code --install-extension ms-dotnettools.csharp
+code --install-extension ionide.ionide-fsharp
+
+# Web dev
+code --install-extension angular.ng-template
+code --install-extension burkeholland.simple-react-snippets
+code --install-extension eg2.vscode-npm-script
+code --install-extension firefox-devtools.vscode-firefox-debug
+code --install-extension glen-84.sass-lint
+code --install-extension johnpapa.angular-essentials
+code --install-extension nrwl.angular-console
+
+# Windows
+code --install-extension ms-vscode.powershell
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+
+# Other Languages
+code --install-extension ms-python.python
+code --install-extension rust-lang.rust
+code --install-extension sfodje.perlcritic
+code --install-extension sfodje.perltidy
+
+Write-Output ""
+Write-Output "Installing npm global tools"
+Write-Output ""
+npm install -g @angular/cli
+npm install -g sass-lint
+npm install -g typescript
+
+Write-Output ""
+Write-Output "Installing dotnet global tools"
+Write-Output ""
+dotnet tool install -g dotnet-ef
--- windows/4_configure.ps1
+++ windows/4_configure.ps1
@@ -1,56 +1,10 @@
 ### Set-ExecutionPolicy Bypass -Scope Process -Force; .\4_configure.ps1
 
-Write-Output ""
-Write-Output "Installing VS Code extensions"
-
-# Common
-code --install-extension coenraads.bracket-pair-colorizer-2
-code --install-extension davidanson.vscode-markdownlint
-code --install-extension eamodio.gitlens
-code --install-extension editorconfig.editorconfig
-code --install-extension esbenp.prettier-vscode
-code --install-extension johnpapa.vscode-peacock
-code --install-extension ms-vsliveshare.vsliveshare
-code --install-extension pkief.material-icon-theme
-code --install-extension robbowen.synthwave-vscode
-
-# .NET
-code --install-extension adrianwilczynski.user-secrets
-code --install-extension ms-dotnettools.csharp
-code --install-extension ionide.ionide-fsharp
-
-# Web dev
-code --install-extension angular.ng-template
-code --install-extension burkeholland.simple-react-snippets
-code --install-extension eg2.vscode-npm-script
-code --install-extension firefox-devtools.vscode-firefox-debug
-code --install-extension glen-84.sass-lint
-code --install-extension johnpapa.angular-essentials
-code --install-extension nrwl.angular-console
-
-# Windows
-code --install-extension ms-vscode.powershell
-code --install-extension ms-vscode-remote.vscode-remote-extensionpack
-
-# Other Languages
-code --install-extension ms-python.python
-code --install-extension rust-lang.rust
-code --install-extension sfodje.perlcritic
-code --install-extension sfodje.perltidy
-
-Write-Output ""
-Write-Output "Installing npm global tools"
-npm install -g @angular/cli
-npm install -g sass-lint
-npm install -g typescript
-
-Write-Output ""
-Write-Output "Installing dotnet global tools"
-dotnet tool install -g dotnet-ef
-
 $configDir = "$home\source\devconfig\config"  # Location of my configuration files - change as needed
+
 Write-Output ""
-Write-Output "Restoring the following configuration files"
+Write-Output "Restoring the following configuration files:"
+Write-Output ""
 Copy-Item -Path "$configDir\.gitconfig"     -Destination $home            -PassThru | Split-Path -Leaf
 Copy-Item -Path "$configDir\starship.toml"  -Destination "$home\.config"  -PassThru | Split-Path -Leaf
 Copy-Item -Path "$configDir\Vim\.gvimrc"    -Destination "$home\_gvimrc"  -PassThru | Split-Path -Leaf
