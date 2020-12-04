8b6e61f98140e5e5cacfe545fc0cd289c595ff08
Edward DeVries <ehdevries@gmail.com>
2020-12-04 11:55:59 -0500

    Install Microsoft Remote Desktop

--- windows/2_install-part-two.ps1
+++ windows/2_install-part-two.ps1
@@ -17,6 +17,7 @@ Start-Process https://www.mozilla.org/en-US/firefox/new/
 Start-Process https://desktop.github.com/
 Start-Process https://insomnia.rest/
 Start-Process https://www.linqpad.net/
+Start-Process https://www.microsoft.com/en-us/p/microsoft-remote-desktop/9wzdncrfj3ps
 Start-Process https://www.nushell.sh/installation.html
 Start-Process https://github.com/coreybutler/nvm-windows
 Start-Process https://www.microsoft.com/en-us/p/onenote-for-windows-10/9wzdncrfhvjl
