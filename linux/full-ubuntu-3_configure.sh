24f920fdb63e0c6cb2503eaa33f5392a5bb47b28
Edward DeVries <ehdevries@gmail.com>
2021-08-31 13:35:56 -0400

    Don't bother with the Angular Essentials pack
    I already install most of those extensions explicitly

--- linux/full-ubuntu-3_configure.sh
+++ linux/full-ubuntu-3_configure.sh
@@ -36,7 +36,6 @@ code --install-extension burkeholland.simple-react-snippets
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eg2.vscode-npm-script
 code --install-extension firefox-devtools.vscode-firefox-debug
-code --install-extension johnpapa.angular-essentials
 code --install-extension nrwl.angular-console
 
 # Docker
