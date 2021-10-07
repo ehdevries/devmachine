11984aa46f6f66a77e8b01591899bab98261be31
Edward DeVries <ehdevries@gmail.com>
2021-10-07 11:03:11 -0400

    Don't install Chocolatey

--- windows/0_install-chocolatey.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-PowerShell -NoProfile -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""& {Invoke-Expression ((New-Object System.Net.WebClient).DownloadString(''https://chocolatey.org/install.ps1'')) ; Write-Output '''' ; Read-Host -Prompt ''Done - Please Press Enter''}""' -Verb RunAs}"
