f3eb3dfa299af1eb0030e0069c603e2ce4ace6d0
Edward DeVries <ehdevries@gmail.com>
2021-10-07 10:14:11 -0400

    Install Nushell with winget

--- windows/2_install-part-two.ps1
+++ windows/2_install-part-two.ps1
@@ -4,6 +4,7 @@ Write-Output ""
 Write-Output "Installing software with Windows Package Manager"
 Write-Output ""
 winget install --id notepad++.notepad++
+winget install --id nushell.nushell
 
 Write-Output ""
 Write-Output "Installing PowerShellGet for Windows PowerShell"
@@ -29,7 +30,6 @@ Start-Process https://desktop.github.com/
 Start-Process https://insomnia.rest/
 Start-Process https://www.linqpad.net/
 Start-Process https://www.microsoft.com/en-us/p/microsoft-remote-desktop/9wzdncrfj3ps
-Start-Process https://www.nushell.sh/
 Start-Process https://github.com/coreybutler/nvm-windows
 Start-Process https://www.microsoft.com/en-us/p/onenote-for-windows-10/9wzdncrfhvjl
 Start-Process https://www.papercut-smtp.com/
