c044970f842700d95df79ed5d1a20362b51d60cb
Edward DeVries <ehdevries@gmail.com>
2021-10-07 11:05:09 -0400

    Simplify WSL installation
    There is now a `wsl --install` command that does it all

--- windows/1_install-as-admin.ps1
+++ windows/1_install-as-admin.ps1
@@ -2,6 +2,7 @@
 ### 2) Set-ExecutionPolicy Bypass -Scope Process -Force; .\1_install-as-admin.ps1
 
 Write-Output ""
-Write-Output "Enabling Windows Subsystem for Linux 2"
+Write-Output "Installing Windows Subsystem for Linux 2"
 Write-Output ""
+wsl --install
 wsl --set-default-version 2
