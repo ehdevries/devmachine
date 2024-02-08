00e2325f0bc7f2975f0bc4cc20b82d366fc2f4db
Edward DeVries <ehdevries@gmail.com>
2024-02-08 12:36:35 -0500

    Don't show those annoying ^M line endings in Git diffs

--- windows/5_configure.ps1
+++ windows/5_configure.ps1
@@ -38,6 +38,7 @@ Write-Output ""
 Write-Output "Adding Windows-specific Git configuration"
 git config --global core.autocrlf true
 git config --global core.editor hx
+git config --global core.whitespace cr-at-eol
 
 Write-Output ""
 Write-Output "All done!"
