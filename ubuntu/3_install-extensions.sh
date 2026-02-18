5e9b3e1d2ed14b982fad92cd181bdb6c350edd9a
Edward DeVries <ehdevries@gmail.com>
2026-02-18 15:13:46 -0500

    Install the Roslyn Language Server global tool

--- ubuntu/3_install-extensions.sh
+++ ubuntu/3_install-extensions.sh
@@ -81,6 +81,7 @@ dotnet tool install -g csharprepl
 dotnet tool install -g dotnet-ef
 dotnet tool install -g dotnet-tools-outdated
 dotnet tool install -g rnwood.smtp4dev
+dotnet tool install -g roslyn-language-server --prerelease
 
 echo ""
 echo "Trusting dotnet dev certificate"
