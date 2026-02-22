34a1b28a6e7a14cf48eaf1a6327dbc0e8bb7e207
Edward DeVries <ehdevries@gmail.com>
2026-02-22 16:04:20 -0500

    Add a minimal starting script for Ubuntu, update docs

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
+_I'm updating the way I run my scripts. This document is a work in progress._
+
 Here are some scripts to make my life a little easier when setting up and configuring my development machine.
+
+## Ubuntu
+
+- Install the software that you'll need in order to run the remaining scripts:
+
+  ```bash
+  chmod +x install-ubuntu-essentials.sh && ./install-ubuntu-essentials.sh
+  ```
 
 ## Windows
 
-- If it's not installed already, install Windows Terminal from the Microsoft Store: <https://aka.ms/terminal>
+- If it's not installed already, install Windows Terminal: <https://aka.ms/terminal>
 
 - Install the software that you'll need in order to run the remaining scripts:
 
---
+++ install-ubuntu-essentials.sh
@@ -0,0 +1,41 @@
+### Before you run this script, make it executable: chmod +x install-ubuntu-essentials.sh
+
+echo
+echo "Adding Git package repository"
+echo
+sudo add-apt-repository ppa:git-core/ppa
+
+echo
+echo "Updating all existing packages"
+echo
+sudo apt update && sudo apt upgrade
+
+echo
+echo "Installing essential packages"
+echo
+sudo apt install build-essential curl file git procps unzip
+
+echo
+echo "Installing Homebrew"
+echo
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
+
+echo
+echo "Installing Nushell"
+echo
+brew install nushell
+
+echo
+echo "Setting environment variables"
+echo
+echo '
+# Set Helix as the default editor
+export EDITOR="hx"
+export VISUAL="hx"
+
+# Opt out of dotnet CLI telemetry
+export DOTNET_CLI_TELEMETRY_OPTOUT=true
+' >> ~/.profile
